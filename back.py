6b66fa81ed46e634597e1c7c6b3ae36a655b2cb8
franciol <fran.aveiro@hotmail.com>
2019-02-27 20:33:06 +0000

    Adicionando esqueleto do Back

---
+++ back.py
@@ -0,0 +1,100 @@
+class calcQTA():
+
+    def __init__(self):
+        self.__numberOfCalls = 22
+
+    def getNumberOfCalculations(self):
+        return self.__numberOfCalls
+
+    def getListOfPossibilities(self):
+        str = '''
+            _-_-Geladeira-_-_\ngeladeira() \nOutputs: \n
+            _-_-Freezer-_-_\nfreeaer() \nOutputs: \n
+            _-_-Lampada-_-_\nlampada() \nOutputs: \n
+            _-_-Televisao-_-_\ntv() \nOutputs: \n
+            _-_-Lava-roupa-_-_\nlavaRoupa() \nOutputs: \n
+            _-_-Lava-roupa-_-_\nlavaLouca() \nOutputs: \n
+            _-_-Carro-_-_\ncarro() \nOutputs: \n
+            _-_-Barco-_-_\nbarco() \nOutputs: \n
+            _-_-Computador-_-_\ncomputador() \nOutputs: \n
+            _-_-Celular-_-_\ncelular() \nOutputs: \n
+            _-_-Chuveiro-_-_\nchuveiro() \nOutputs: \n
+            _-_-Fogao-_-_\nfogao() \nOutputs: \n
+            _-_-Ferro de Passar Roupa-_-_\nferroPassarRoupa() \nOutputs: \n
+            _-_-Microondas-_-_\nmicroondas() \nOutputs: \n
+            _-_-VideoGame-_-_\nvideoGame() \nOutputs: \n
+            _-_-Secador de Cabelo-_-_\nsecadorCabelo() \nOutputs: \n
+            _-_-Chapinha-_-_\nchapinha() \nOutputs: \n
+            _-_-Ventilador-_-_\nventilador() \nOutputs: \n
+            _-_-Ar Condicionado-_-_\narCondicionado() \nOutputs: \n
+            _-_-Aquecedor-_--\naquecedor() \nOutputs: \n
+            _-_-Vending Machine-_-_\nvendingMachine() \nOutputs: \n
+            _-_-Sistema de Som-_-_\nsistemaSom() \nOutputs: \n
+            '''
+        return str
+
+    def geladeira(self):
+        return 0
+
+    def freezer(self):
+        return 0
+
+    def Lampada(self):
+        return 0
+
+    def tv(self):
+        return 0
+
+    def lavaRoupa(self):
+        return 0
+
+    def lavaLouca(self):
+        return 0
+
+    def carro(self):
+        return 0
+
+    def barco(self):
+        return 0
+
+    def computador(self):
+        return 0
+
+    def celular(self):
+        return 0
+
+    def chuveiro(self):
+        return 0
+
+    def fogao(self):
+        return 0
+
+    def ferroPassarRoupa(self):
+        return 0
+
+    def microondas(self):
+        return 0
+
+    def videoGame(self):
+        return 0
+
+    def secadorCabelo(self):
+        return 0
+
+    def chapinha(self):
+        return 0
+
+    def ventilador(self):
+        return 0
+
+    def arCondicionado(self):
+        return 0
+
+    def aquecedor(self):
+        return 0
+
+    def vendingMachine(self):
+        return 0
+
+    def sistemaSom(self):
+        return 0
